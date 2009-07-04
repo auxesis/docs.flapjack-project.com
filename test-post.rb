f6f6c4d52b8b6447173626d56bb8f3b4025f2936
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-07-05 00:36:42 +0200

    accept an port option in the poster

--- test-post.rb
+++ test-post.rb
@@ -2,7 +2,14 @@
 
 require 'net/http'
 
-http = Net::HTTP.new("localhost", 4567)
+if ARGV.grep(/\-h|\-\-help/).size > 0
+  puts "Usage: test-post.rb [port]"
+  exit 0
+end
+
+port = (ARGV[0] || 4567)
+
+http = Net::HTTP.new("localhost", port)
 response = http.request_post('/', 'payload={"foo": "bar"}')
 
 puts response.read_body
