6ece109c4b4071375ede53f191f5df720cef6888
Lindsay Holmwood <lindsay@holmwood.id.au>
2009-07-05 00:38:03 +0200

    accept host option

--- test-post.rb
+++ test-post.rb
@@ -3,13 +3,14 @@
 require 'net/http'
 
 if ARGV.grep(/\-h|\-\-help/).size > 0
-  puts "Usage: test-post.rb [port]"
+  puts "Usage: test-post.rb [host] [port]"
   exit 0
 end
 
-port = (ARGV[0] || 4567)
+host = (ARGV[0] || 'localhost')
+port = (ARGV[1] || 4567)
 
-http = Net::HTTP.new("localhost", port)
+http = Net::HTTP.new(host, port)
 response = http.request_post('/', 'payload={"foo": "bar"}')
 
 puts response.read_body
